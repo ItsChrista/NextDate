e78e0ff24929be2ac27d4ace118330ddf03224bd
origin <christinekaldoja04@gmail.com>
2025-04-01 22:51:37 +0300

    T1

---
+++ src/test/java/ee/ut/cs/swt/nextdate/GTv1.java
@@ -0,0 +1,162 @@
+package ee.ut.cs.swt.nextdate;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GTv1 {
+
+	@Test(timeout = 4000)
+	public void testCreatesNextDate1() throws Throwable {
+	    NextDate nextDate0 = new NextDate(1, 1, 1);
+	    String string0 = nextDate0.run(1, 31, 1965);
+	    assertEquals("2/1/1965", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testCreatesNextDate0() throws Throwable {
+	    NextDate nextDate0 = new NextDate(5, 5, 5);
+	    String string0 = nextDate0.run(5, 5, 1941);
+	    assertEquals("5/6/1941", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testCreatesNextDate3() throws Throwable {
+	    NextDate nextDate0 = new NextDate(1922, 1922, 1922);
+	    String string0 = nextDate0.run(2, 29, 1922);
+	    assertEquals("Invalid Input Date", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testCreatesNextDate2() throws Throwable {
+	    NextDate nextDate0 = new NextDate(11, 11, 11);
+	    String string0 = nextDate0.run(11, 31, 2000);
+	    assertEquals("Invalid Input Date", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testCreatesNextDate5() throws Throwable {
+	    NextDate nextDate0 = new NextDate(2, 2, 2);
+	    String string0 = nextDate0.run(2, 2, 1817);
+	    assertEquals("2/3/1817", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testCreatesNextDate4() throws Throwable {
+	    NextDate nextDate0 = new NextDate(7, 7, 7);
+	    String string0 = nextDate0.run(7, 7, 1963);
+	    assertEquals("7/7/1963", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testRunReturningNonEmptyString() throws Throwable {
+	    NextDate nextDate0 = new NextDate(3, 15, 15);
+	    String string0 = nextDate0.run(12, 15, 889);
+	    assertEquals("invalid Input Date", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testRunWithZero() throws Throwable {
+	    NextDate nextDate0 = new NextDate(6, 4, 4);
+	    String string0 = nextDate0.run(0, 4, 889);
+	    assertEquals("invalid Input Date", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testRunWithNegativeAndPositive1() throws Throwable {
+	    NextDate nextDate0 = new NextDate(6, 4, 4);
+	    String string0 = nextDate0.run(3394, 6, (-14));
+	    assertEquals("invalid Input Date", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testRunWithNegativeAndPositive0() throws Throwable {
+	    NextDate nextDate0 = new NextDate(6, 4, 4);
+	    String string0 = nextDate0.run(4, (-288), 1);
+	    assertEquals("invalid Input Date", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testCreatesNextDate14() throws Throwable {
+	    NextDate nextDate0 = new NextDate(1, 1, 1);
+	    String string0 = nextDate0.run(10, 5, 1931);
+	    assertEquals("10/6/1931", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testRunWithPositive() throws Throwable {
+	    NextDate nextDate0 = new NextDate(1, 1, 1);
+	    String string0 = nextDate0.run(1, 1, 3340);
+	    assertEquals("invalid Input Date", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testCreatesNextDate13() throws Throwable {
+	    NextDate nextDate0 = new NextDate(0, 0, 0);
+	    String string0 = nextDate0.run(10, 2021, 2021);
+	    assertEquals("invalid Input Date", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testCreatesNextDate16() throws Throwable {
+	    NextDate nextDate0 = new NextDate(12, 12, 12);
+	    String string0 = nextDate0.run(12, 31, 2010);
+	    assertEquals("12/32/2010", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testCreatesNextDate15() throws Throwable {
+	    NextDate nextDate0 = new NextDate(6, (-3), (-3));
+	    String string0 = nextDate0.run(8, 31, 1801);
+	    assertEquals("9/1/1801", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testCreatesNextDate7() throws Throwable {
+	    NextDate nextDate0 = new NextDate(2, 2, 2);
+	    String string0 = nextDate0.run(2, 31, 1984);
+	    assertEquals("Invalid Input Date", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testCreatesNextDate6() throws Throwable {
+	    NextDate nextDate0 = new NextDate(2, 2, 2);
+	    String string0 = nextDate0.run(2, 28, 1984);
+	    assertEquals("3/1/1984", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testCreatesNextDate9() throws Throwable {
+	    NextDate nextDate0 = new NextDate(4, 4, 4);
+	    String string0 = nextDate0.run(4, 4, 1941);
+	    assertEquals("4/5/1941", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testCreatesNextDate8() throws Throwable {
+	    NextDate nextDate0 = new NextDate(3, 3, 3);
+	    String string0 = nextDate0.run(3, 3, 1819);
+	    assertEquals("3/4/1819", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testCreatesNextDate10() throws Throwable {
+	    NextDate nextDate0 = new NextDate(6, 4, 4);
+	    String string0 = nextDate0.run(6, 4, 1820);
+	    assertEquals("6/5/1820", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testCreatesNextDate12() throws Throwable {
+	    NextDate nextDate0 = new NextDate(3, 15, 15);
+	    String string0 = nextDate0.run(12, 15, 1801);
+	    assertEquals("12/16/1801", string0);
+	}
+
+	@Test(timeout = 4000)
+	public void testCreatesNextDate11() throws Throwable {
+	    NextDate nextDate0 = new NextDate(9, 9, 9);
+	    String string0 = nextDate0.run(9, 9, 1804);
+	    assertEquals("9/10/1804", string0);
+	}
+
+}
